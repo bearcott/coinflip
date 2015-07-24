d455ec7d84b640c08e2ff1d7895fc530f6ebc639
Bear Cott <bear@bearcott.com>
2015-07-24 17:11:55 -0500

    Initial Commit

---
+++ coinflip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coinflip
+//
+//  Created by Benny Yan on 7/24/15.
+//  Copyright (c) 2015 ___bear___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
