a77fb6f86ed70dcc5bb4725c5dca45ed94f64165
Bear Cott <bear@bearcott.com>
2015-07-24 20:39:33 -0500

    Main components finished

--- coinflip/ViewController.swift
+++ coinflip/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   @IBOutlet weak var words: UILabel!
+   @IBAction func coin(sender: AnyObject) {
+      var subject = ["bro","mate","dude","man",":)"];
+      var which = arc4random_uniform(UInt32(subject.count));
+      
+      var content = "";
+      
+      var decision = arc4random_uniform(2)
+      if (decision == 0) {
+         content = "Its heads \(subject[Int(which)])!";
+      }else{
+         content = "Its tails mate!";
+      }
+      var mcontent = NSMutableAttributedString();
+      mcontent = NSMutableAttributedString(string: content, attributes: [NSFontAttributeName:UIFont(name: "Helvetica-Bold", size: 36.0)!]);
+      mcontent.addAttribute(NSForegroundColorAttributeName, value: UIColor.redColor(), range: NSRange(location:4,length:5))
+      words.attributedText = mcontent;
+   }
    override func viewDidLoad() {
       super.viewDidLoad()
       // Do any additional setup after loading the view, typically from a nib.
+      self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!);
    }
 
    override func didReceiveMemoryWarning() {
